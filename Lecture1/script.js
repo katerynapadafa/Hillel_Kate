fd141607fba36a50e677b1cca6d2dae9645f0321
katerynapadafa <padafa2000@gmail.com>
2022-03-26 15:45:37 +0200

    added hw2

--- Lecture1/script.js
+++ Lecture1/script.js
@@ -1,3 +1,4 @@
 let userName = prompt('What is your name?');
-let message = 'Hello, ' + userName + '! How are you?'
-alert(message)
+let message = 'Hello, ' + userName + '! How are you?';
+
+alert(message);
---
+++ Lecture2/script.js
@@ -0,0 +1,30 @@
+function calculator() {
+
+    const mathOperation = prompt('Which math operation would you like to do? (Type +, -, * or /)');
+    const firstNumber = +prompt('Enter first number');
+    const secondNumber = +prompt('Enter second number');
+
+    const result = (`${firstNumber} ${mathOperation} ${secondNumber} =`);
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        return alert('Use numbers only!');
+    };
+
+    switch (mathOperation) {
+        case '+':
+            alert(`${result} ${firstNumber + secondNumber}`);
+            break;
+        case '-':
+            alert(`${result} ${firstNumber - secondNumber}`);
+            break;
+        case '*':
+            alert(`${result} ${firstNumber * secondNumber}`);
+            break;
+        case '/':
+            alert(`${result} ${firstNumber / secondNumber}`);
+            break;
+        default:
+            alert('Invalid math operation');
+            break;
+    };
+};
+calculator();
