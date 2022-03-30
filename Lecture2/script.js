b5eac6bdbb6a6a46a5bb0eefa7d30402acfb372a
katerynapadafa <padafa2000@gmail.com>
2022-03-30 19:19:08 +0300

    hw3

--- Lecture2/script.js
+++ Lecture2/script.js
@@ -3,8 +3,8 @@ function calculator() {
     const mathOperation = prompt('Which math operation would you like to do? (Type +, -, * or /)');
     const firstNumber = +prompt('Enter first number');
     const secondNumber = +prompt('Enter second number');
+    const result = `${firstNumber} ${mathOperation} ${secondNumber} =`;
 
-    const result = (`${firstNumber} ${mathOperation} ${secondNumber} =`);
     if (isNaN(firstNumber) || isNaN(secondNumber)) {
         return alert('Use numbers only!');
     };
---
+++ Lecture3/script.js
@@ -0,0 +1,45 @@
+const mathOperator = getMathOperator();
+const firstNumber = getNumber('Enter first number');
+const secondNumber = getNumber('Enter second number');
+const result = calculate(firstNumber, secondNumber, mathOperator);
+showFinalResult(firstNumber, secondNumber, mathOperator, result);
+
+function getMathOperator() {
+    const operator = prompt('Which math operation would you like to do? (Type +, -, * or /)');
+    if (operator !== '+' &&
+        operator != '-' &&
+        operator != '*' &&
+        operator != '/') {
+        alert('Invalid math operator! Type +, -, * or /.');
+        return getMathOperator();
+    }
+    return operator;
+}
+
+function getNumber(message) {
+    const userNumber = +prompt(message);
+    if (isNaN(userNumber)) {
+        alert('Use numbers only!');
+        return getNumber(message);
+    }
+    return userNumber;
+}
+
+function calculate(firstNumber, secondNumber, mathOperator) {
+    switch (mathOperator) {
+        case '+':
+            return firstNumber + secondNumber;
+        case '-':
+            return firstNumber - secondNumber;
+        case '*':
+            return firstNumber * secondNumber;
+        case '/':
+            return firstNumber / secondNumber;
+        default:
+            alert('Invalid math operation');
+    }
+}
+
+function showFinalResult(firstNumber, secondNumber, mathOperator, result) {
+    alert(`Your result: ${firstNumber} ${mathOperator} ${secondNumber} = ${result}`)
+}
