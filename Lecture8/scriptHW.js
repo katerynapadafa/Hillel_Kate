65de8e93727277ed2c8edc00de1633fbcf68e2e2
katerynapadafa <padafa2000@gmail.com>
2022-04-16 20:15:55 +0300

    improved hw8

--- Lecture8/scriptHW.js
+++ Lecture8/scriptHW.js
@@ -2,10 +2,12 @@ const firstNumberEl = document.getElementById('first-number');
 const secondNumberEl = document.getElementById('second-number');
 const calculateBtnEl = document.querySelector('.calculate-btn');
 const resultTextEl = document.querySelector('.result-text')
+const errorEl = document.querySelector('.error-text')
 const operatorEl = document.querySelector('select')
 
 calculateBtnEl.addEventListener('click', onCalculateBtn);
-firstNumberEl.addEventListener('click', onInput)
+firstNumberEl.addEventListener('input', getValue1)
+secondNumberEl.addEventListener('input', getValue2)
 
 function onCalculateBtn() {
     calculate();
@@ -13,16 +15,10 @@ function onCalculateBtn() {
 }
 
 function calculate() {
-    const a = getValue(firstNumberEl);
-    const b = getValue(secondNumberEl);
-    const op = getValue(operatorEl);
-    if (a === '' || b === '' || a === null || b === null) {
-        return resultTextEl.textContent = 'Please, check your numbers!';
-    };
-    if (isNaN(a) || isNaN(b)) {
-        return resultTextEl.textContent = 'Please, use numbers only!';
-    };
-    let result;
+    const a = getValue1();
+    const b = getValue2();
+    const op = operatorEl.value;
+    let result = 0;
     switch (op) {
         case '+':
             result = Number(a) + Number(b);
@@ -37,11 +33,31 @@ function calculate() {
             result = a / b;
             break;
     }
-    return resultTextEl.textContent = `Your result = ${result} `
+    return resultTextEl.textContent = `Your result = ${result}`
 }
 
-function getValue(element) {
-    return element.value;
+function getValue1() {
+    const val1 = firstNumberEl.value;
+    if (val1 === '' || val1 === null) {
+        return errorEl.textContent = 'Please, check your first number!';
+    }
+    if (isNaN(val1)) {
+        return errorEl.textContent = 'Your first element is not a number!';
+    }
+    errorEl.textContent = '';
+    return val1
+}
+
+function getValue2() {
+    const val2 = secondNumberEl.value;
+    if (val2 === '' || val2 === null) {
+        return errorEl.textContent = 'Please, check your second number!';
+    }
+    if (isNaN(val2)) {
+        return errorEl.textContent = 'Your second element is not a number!';
+    }
+    errorEl.textContent = '';
+    return val2
 }
 
 function clearInput() {
