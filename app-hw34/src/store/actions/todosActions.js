a77835821c7ce590a62748f248698779c7fd399e
katerynapadafa <padafa2000@gmail.com>
2022-08-05 11:10:00 +0300

    hw 35

--- app-hw34/src/store/actions/todosActions.js
+++ app-hw34/src/store/actions/todosActions.js
@@ -1,16 +1,14 @@
 export const TODOS_TOGGLE_TODO =' TODOS_TOGGLE_TODO'
-
-export function toggleTodo(id){
-    return {type: TODOS_TOGGLE_TODO, payload: id}
-}
-
+export const toggleTodo= createAction(TODOS_TOGGLE_TODO)
+  
 export const TODOS_REMOVE_TODO =' TODOS_REMOVE_TODO'
+export const removeTodo= createAction(TODOS_REMOVE_TODO)
 
-export function removeTodo(id){
-    return {type: TODOS_REMOVE_TODO, payload: id}
-}
 export const TODOS_CREATE_TODO =' TODOS_CREATE_TODO'
+export const createTodo= createAction(TODOS_CREATE_TODO)
 
-export function createTodo(newTodo){
-    return {type: TODOS_CREATE_TODO, payload: newTodo}
+function createAction(type){
+   return(payload) =>{
+    return {type, payload}
+   }
 }
